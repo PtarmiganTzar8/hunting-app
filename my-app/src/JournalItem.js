4a195f4481e991f02145b5d8c2556892b3ce8b6f
Michael <michael.d.lange88@gmail.com>
2023-06-19 15:20:09 -0700

    Finish the maping over each excursion

--- my-app/src/JournalItem.js
+++ my-app/src/JournalItem.js
@@ -2,12 +2,22 @@ import React from "react";
 
 function JournalItem({ excursion }) {
     const { id, date, location, bag, guide, comments } = excursion;
-
+    console.log(bag.Mallard)
 
     return(
-        <li>
-            <h3></h3>
-        </li> 
+        <ul>
+            <h2>Date: {date}</h2>
+            <h3>Location: {location}</h3>
+            <h4>
+                Mallards: {bag.Mallard}<br></br>
+                Widgeons: {bag.Widgeon}<br></br>
+                Canvasback: {bag.Canvasback}<br></br>
+                Gadwall: {bag.Gadwall}<br></br>
+                Other: {bag.Other}<br></br>
+            </h4>
+            <h5>Guide: {guide}</h5>
+            <p>Comments: {comments}</p>
+        </ul> 
     )
 }
 
